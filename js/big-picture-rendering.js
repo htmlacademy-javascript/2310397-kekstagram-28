610785ef1dc89e4beec174583ed14f3b8023a3fd
Александр Клюйко <alexandar_97_28@mail.ru>
2023-03-29 00:10:50 +0300

    8.15. Открывается и закрывается (часть 2). Пробный вариант

--- js/big-picture-rendering.js
+++ js/big-picture-rendering.js
@@ -7,7 +7,7 @@ const bigPictureImage = bigPhoto.querySelector('.big-picture__img');
 const likesCount = bigPhoto.querySelector('.likes-count');
 const commentsCount = bigPhoto.querySelector('.comments-count');
 const photoDescription = bigPhoto.querySelector('.social__caption');
-const currentCommentsCount = bigPhoto.querySelector('.social__comment-count');
+
 const commentsLoader = bigPhoto.querySelector('.comments-loader');
 
 const bigPhotoCancel = bigPhoto.querySelector('.big-picture__cancel');
@@ -38,7 +38,10 @@ const createBigPhotoDescription = (photo) => {
   commentsCount.textContent = photo.comments.length;
   photoDescription.textContent = photo.description;
   // Добавление комментариев под большое фото из модуля 'render-comments.js'
-  renderComments(photo.comments);
+  // renderDefaultCommentCount(photo.comments);
+  // commentsLoader.addEventListener('click', () => {
+  //   onCommentsLouderButtonClick(photo.comments);
+  // });
 };
 
 
@@ -46,12 +49,36 @@ const renderBigPicture = (photos) => {
   bigPhoto.classList.remove('hidden');
   createBigPhotoDescription(photos);
 
-  currentCommentsCount.classList.add('hidden');
-  commentsLoader.classList.add('hidden');
+  // currentCommentsCount.classList.add('hidden');
+  // commentsLoader.classList.add('hidden');
   document.querySelector('body').classList.add('modal-open');
 
   bigPhotoCancel.addEventListener('click', onCrossClick);
   document.addEventListener('keydown', onDocumentEscKeydown);
+  let currentCommentsShown = 5;
+
+  const renderDefaultCommentCount = () => {
+    currentCommentsShown = 5;
+    commentsLoader.classList.remove('hidden');
+    renderComments(photos.comments, currentCommentsShown);
+    // console.log(currentCommentsShown);
+  };
+
+  const onCommentsLouderButtonClick = () => {
+    currentCommentsShown += currentCommentsShown;
+
+    if (currentCommentsShown >= photos.comments.length) {
+      commentsLoader.classList.add('hidden');
+      currentCommentsShown = photos.comments.length;
+    } else {
+      commentsLoader.classList.remove('hidden');
+    }
+    // console.log(currentCommentsShown);
+    renderComments(photos.comments, currentCommentsShown);
+  };
+
+  renderDefaultCommentCount();
+  commentsLoader.addEventListener('click', onCommentsLouderButtonClick);
 };
 
 export {renderBigPicture};
--- js/render-comments.js
+++ js/render-comments.js
@@ -1,18 +1,44 @@
 const listComments = document.querySelector('.social__comments');
+const commentTemplate = listComments.querySelector('li');
+const bigPhoto = document.querySelector('.big-picture');
+const currentCommentsCount = bigPhoto.querySelector('.social__comment-count');
+const commentsLoader = bigPhoto.querySelector('.comments-loader');
 
-const renderComments = (comments) => {
+
+// let currentCommentsShown = 5;
+
+// const createComment = (comment) => {
+//   const commentElement = commentTemplate.cloneNode(true);
+//   commentElement.querySelector('.social__picture').src = comment.avatar;
+//   commentElement.querySelector('.social__picture').alt = comment.name;
+//   commentElement.querySelector('.social__text').textContent = comment.message;
+// };
+
+
+// const renderComments = () => {
+//   currentCommentsShown += currentCommentsShown;
+
+//   if (currentCommentsShown >= comments.length)
+// };
+
+
+
+const renderComments = (comments, currentCommentsShown) => {
   const commentListFragment = document.createDocumentFragment();
-  const commentTemplate = listComments.querySelector('li');
   listComments.innerHTML = '';
 
-  comments.forEach(({avatar, name, message}) => {
+  for (let i = 0; i < currentCommentsShown; i++) {
     const commentElement = commentTemplate.cloneNode(true);
-    commentElement.querySelector('.social__picture').src = avatar;
-    commentElement.querySelector('.social__picture').alt = name;
-    commentElement.querySelector('.social__text').textContent = message;
+    commentElement.querySelector('.social__picture').src = comments[i].avatar;
+    commentElement.querySelector('.social__picture').alt = comments[i].name;
+    commentElement.querySelector('.social__text').textContent = comments[i].message;
     commentListFragment.appendChild(commentElement);
-  });
+  }
+  // console.log(commentListFragment.children[1]);
   listComments.appendChild(commentListFragment);
 };
+
+
+
 
 export {renderComments};
